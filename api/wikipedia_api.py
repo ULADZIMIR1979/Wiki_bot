8d17e7c3523d4e75085aeffab55373d3da047dd8
ULADZIMIR1979 <168981426+ULADZIMIR1979@users.noreply.github.com>
2024-10-03 19:43:45 +0300

    Update wikipedia_api.py

--- api/wikipedia_api.py
+++ api/wikipedia_api.py
@@ -8,6 +8,8 @@ def get_wiki_info(query):
         page_url = wikipedia.page(query).url
         return summary, page_url
 
-    except wikipedia.exceptions.WikipediaException as e:
-        return None, f'{e}\nНе удалось найти информацию по запросу. Проверьте, правильно ли введён запрос.'
+    except wikipedia.exceptions.WikipediaException:
+        return None, (f'Не удалось найти информацию по вашему запросу.\n'
+                      f'Проверьте, правильно ли введён запрос.\n'
+                      f'Нажмите на кнопку **Search** и введите ваш запрос ещё раз.')
 
